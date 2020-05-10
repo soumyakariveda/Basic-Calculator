957fde8a971f5ff3a20e3979b626ea97b6e762df
Soumya Kariveda <karivedasoumya@gmail.com>
2020-05-10 16:21:15 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -3,25 +3,17 @@ import java.io.*;
 import java.util.*;
 
 public class App {
-  public static double addition(double a,double b)
-  {
-    return a+b;
-  }
-  public static double subtraction(double a,double b)
-  {
-    return a-b;
-  }
    public static void main(String[] args) {
       double num1;
       double num2;
       double ans;
       char op;
       String s;
-//       Scanner in = new Scanner(System. in);
-//       System. out. println("Enter an equation (like 3+4) ");
-//       s = in. nextLine();
-//       Scanner reader = new Scanner(System.in);
-      s="3*4";
+      //Scanner in = new Scanner(System. in);
+      //System. out. println("Enter an equation (like 3+4) ");
+      //s = in. nextLine();
+      //Scanner reader = new Scanner(System.in);
+      s="3+4";
       String t="";
       int i=0;
       while(i<s.length() && s.charAt(i)!='+' && s.charAt(i)!='/' && s.charAt(i)!='-' && s.charAt(i)!='*' )
@@ -30,7 +22,6 @@ public class App {
         t+=c;
         i++;
       }
-      //System. out. println(t);
       num1 = Double.valueOf(t);
       op = s.charAt(i);
       i++;
@@ -39,9 +30,9 @@ public class App {
         t+=s.charAt(j);
       num2 = Double.valueOf(t);
       switch(op) {
-         case '+': ans = addition(num1,num2);
+         case '+': ans = num1+num2;
             break;
-         case '-': ans = subtraction(num1,num2);
+         case '-': ans = num1-num2;
             break;
          case '*': ans = num1 * num2;
             break;
