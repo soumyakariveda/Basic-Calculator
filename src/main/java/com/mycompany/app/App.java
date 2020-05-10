b6500805b335ce29ea8bf51cf57f045d9ac3e3b6
Soumya Kariveda <karivedasoumya@gmail.com>
2020-05-10 15:02:58 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -17,10 +17,11 @@ public class App {
       double ans;
       char op;
       String s;
-      Scanner in = new Scanner(System. in);
-      System. out. println("Enter an equation (like 3+4) ");
-      s = in. nextLine();
-      Scanner reader = new Scanner(System.in);
+      //Scanner in = new Scanner(System. in);
+      //System. out. println("Enter an equation (like 3+4) ");
+      //s = in. nextLine();
+      //Scanner reader = new Scanner(System.in);
+      s="3+4";
       String t="";
       int i=0;
       while(i<s.length() && s.charAt(i)!='+' && s.charAt(i)!='/' && s.charAt(i)!='-' && s.charAt(i)!='*' )
@@ -29,7 +30,7 @@ public class App {
         t+=c;
         i++;
       }
-      System. out. println(t);
+      //System. out. println(t);
       num1 = Double.valueOf(t);
       op = s.charAt(i);
       i++;
@@ -49,7 +50,7 @@ public class App {
          default:  System.out.printf("Error! Enter correct operator");
             return;
       }
-      System.out.print("\nThe result is given as follows:\n");
-      System.out.printf(num1 + " " + op + " " + num2 + " = " + ans);
+      System.out.print("The result is given as follows:\n");
+      System.out.printf(num1 + " " + op + " " + num2 + " = " + ans+"\n");
    }
 }
